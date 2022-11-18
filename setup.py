03d353c25e576e7a12bf4967bd5ed46b9335abe1
Laurenz Tolentino <laurenz@outlook.ph>
2022-11-18 23:13:54 +0800

    [1.0.0] Test import

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     ,description='Thesis code'
     ,author='Laurenz Tolentino'
     ,author_email='laurenz@outlook.ph'
-    ,packages=['src/ecg_denoiser_laurenz']
+    ,packages=['src/ecg_denoiser_laurenz', "src/ecg_denoiser_laurenz/ecg_toolkit", "src/ecg_denoiser_laurenz/models"]
     ,install_requires=['numpy', 'torch', 'matplotlib', 'sys', 'cython']
 )
